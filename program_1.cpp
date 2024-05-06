5657a4ba5e0ca42d46fa08f54cefc6ddfd237d2e
aircongo <natelufuluabo@yahoo.ca>
2024-05-06 13:15:33 -0400

    added the ability to calculate total budget with all buyers monthly budget

--- program_1.cpp
+++ program_1.cpp
@@ -22,4 +22,49 @@ void print_banniere()
 int main()
 {
     print_banniere();
+
+    int nombre_dacheteurs = 0;
+    float budget_total;
+    float budgets_coproprietaires[nombre_dacheteurs];
+    float prix_propriete;
+    float mise_de_fonds;
+    float interet_annuel;
+    int periode_amortissement;
+
+    bool faire_simulation = true;
+
+    while (faire_simulation)
+    {
+        do
+        {
+            std::cout << "Combien de personnes contribueront à l'achat de la propriété? ";
+            std::cin >> nombre_dacheteurs;
+        } while (nombre_dacheteurs <= 0);
+
+        std::cout << nombre_dacheteurs << std::endl;
+
+        if (nombre_dacheteurs == 1)
+        {
+            do
+            {
+                std::cout << "Entrez le budget mensuel ($): ";
+                std::cin >> budget_total;
+            } while (budget_total <= 0);
+        }
+        else
+        {
+            for (int i = 0; i < nombre_dacheteurs; i++)
+            {
+                do
+                {
+                    std::cout << "Entrez le budget mensuel du copropriétaire " << i + 1 << " ($): ";
+                    std::cin >> budgets_coproprietaires[i];
+                } while (budgets_coproprietaires[i] <= 0);
+
+                budget_total += budgets_coproprietaires[i];
+            }
+        }
+
+        std::cout << "Votre budget total est: " << budget_total << std::endl;
+    }
 }
