94320a8e632175a8ad68fccb3dfe1172b54e968f
aircongo <natelufuluabo@yahoo.ca>
2024-05-10 15:58:28 -0400

    rewrote affichages.h

--- program_2/affichages.h
+++ program_2/affichages.h
@@ -1,18 +1,10 @@
 #include <string>
 #include <iostream>
 
-void imprimer_hash(int largeur_banniere)
-{
-    for (int i = 0; i < largeur_banniere; i++)
-    {
-        printf("#");
-    }
-}
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
-void affiche_banniere(std::string titre)
-{
-    int largeur_banniere = titre.length() + 20;
-    imprimer_hash(largeur_banniere);
-    printf("titre: ");
-    imprimer_hash(largeur_banniere);
-}
+void imprimer_hash(int largeur_banniere);
+void affiche_banniere(std::string titre);
+
+#endif
