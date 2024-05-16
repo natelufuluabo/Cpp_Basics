a928dbab31bcc1c30c9a0b9770cced89b27d1933
aircongo <natelufuluabo@yahoo.ca>
2024-05-15 20:22:36 -0400

    declared 2 functions in the utilitaires.h file

---
+++ program_2/utilitaires.h
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int convertir_dollars_vers_centimes(float montant_en_dollars);
+std::string formater_argent(int montant_en_centimes);
+
+#endif
