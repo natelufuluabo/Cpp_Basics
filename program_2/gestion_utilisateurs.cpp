485ebd099a5b4dd8ab07a301175339b5117762a6
aircongo <natelufuluabo@yahoo.ca>
2024-05-24 00:02:14 -0400

    implemented function that checks if username already exists

--- program_2/gestion_utilisateurs.cpp
+++ program_2/gestion_utilisateurs.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <array>
 #include "gestion_utilisateurs.h"
 #include "configurations.h"
 
 using namespace std;
+
+bool utilisateur_existe(std::string p_nom_utilisateur)
+{
+    ifstream fichier(fichier_utilisateurs);
+    string nom_utilisateur = "";
+    string ligne;
+
+    while(getline(fichier, ligne))
+    {
+        for (int i = 0; i < ligne.length(); i++)
+        {
+            if (ligne[i] != ',')
+            {
+                nom_utilisateur += ligne[i];
+            }
+            else 
+            {
+                break;
+            }
+        }
+
+        if (p_nom_utilisateur == nom_utilisateur) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 bool enregistrer_utilisateur()
 {
@@ -14,9 +43,9 @@ bool enregistrer_utilisateur()
 
     cout << "Entrez le nom d'utilisateur: ";
     cin >> entree_utilisateur;
-    if (entree_utilisateur.length() < 1)
+    if (utilisateur_existe(entree_utilisateur))
     {
-        cout << "Le nom d'utilisateur ne peut pas être vide." << endl;
+        cout << "Le nom d'utilisateur existe déjà." << endl;
         return false;
     }
     nom_utilisateur =  entree_utilisateur;
@@ -24,11 +53,6 @@ bool enregistrer_utilisateur()
 
     cout << "Entrez le mot de passe: ";
     cin >> entree_utilisateur;
-    if (entree_utilisateur.length() < 1)
-    {
-        cout << "Le mot de passe ne peut pas être vide" << endl;
-        return false;
-    }
     mot_de_passe = entree_utilisateur;
     cin.ignore();
 
